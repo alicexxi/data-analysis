2e3e337c7201383ca783d0cec272256583452cbc
alicexxi <hanxike@uw.edu>
2024-10-19 11:31:11 -0700

    add hw2

--- HW2.py
+++ HW2.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use('Agg') 
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
@@ -21,13 +23,13 @@ for modes in range(1, 6):
     depsilon = 0.2
 
     for _ in range(1000):  
-        y0 = [1, K * L ** 2 - epsilon]; 
+        y0 = [1, np.sqrt(K * L ** 2 - epsilon)]; 
         y = odeint(shoot, y0, xshoot, args=(K,epsilon,)) 
 
         if abs(y[-1, 1] + np.sqrt(K * L**2 - epsilon) * y[-1, 0]) < tol:  
             break 
 
-        if ((-1) ** (modes + 1) * y[-1, 1] + np.sqrt(K * L**2 - epsilon) * y[-1, 0]) > 0:
+        if ((-1) ** (modes + 1) * (y[-1, 1] + np.sqrt(K * L**2 - epsilon) * y[-1, 0])) > 0:
             epsilon += depsilon
         else:
             epsilon -= depsilon 
@@ -38,7 +40,7 @@ for modes in range(1, 6):
     eigenfuction = abs(y[:, 0] / np.sqrt(norm))
     A1[:, modes - 1] = eigenfuction
     
-    plt.plot(xshoot, y[:, 0] / np.sqrt(norm), col[modes - 1])  
+    plt.plot(xshoot, eigenfuction, col[modes - 1])  
 
 plt.show() 
 print(A1)
